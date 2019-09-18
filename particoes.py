9b6a1ac454fb569b78d58b71f96528748c06126f
Shellemc <54825823+Shellemc@users.noreply.github.com>
2019-09-17 23:23:42 -0300

    Delete particoes.py

--- particoes.py
+++
@@ -1,7 +0,0 @@
-from os import system
-
-part = ['fdisk -l | sed -n 1p | cut -d: -f2 | cut -d, -f1 | tr -d a-zA-Z" "']
-nome = ["fdisk -l | sed -n 1p | grep '/dev/...' | sed 's/.*\\/dev\\///g' | sed 's/\\:.*//g'"]
-raiz = [f'echo "{part}" - 2 | bc']
-
-print(f'raiz: {raiz} | part: {part}')
